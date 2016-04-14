2e8ad4c2cb76bb33c372ec19df4a06881763c523
Luke Horvat <lukehorvat@gmail.com>
2016-04-14 21:51:26 +1000

    Separate CLI-specific code

--- index.js
+++ index.js
@@ -1,55 +1,28 @@
-#!/usr/bin/env node
-
 "use strict";
 
-var pkg = require("./package.json");
 var path = require("path");
-var chalk = require("chalk");
 var template = require("lodash.template");
 var map = require("map-stream");
-var tildify = require("tildify");
-var vfs = require("vinyl-fs");
-var yargs = require("yargs");
-var argv = yargs
-  .usage("Usage: $0 <dir>")
-  .demand(0, 1)
-  .option("h", { alias: "help", describe: "Show help" })
-  .option("v", { alias: "version", describe: "Show version" })
-  .argv;
+var fs = require("vinyl-fs");
 
-if (argv.help || argv.h) {
-  yargs.showHelp();
-  process.exit();
-}
+module.exports = function(dir) {
+  return new Promise(function(resolve, reject) {
+    var files = [];
 
-if (argv.version || argv.v) {
-  console.log(pkg.version);
-  process.exit();
-}
-
-var dir = process.cwd();
-if (argv._.length > 0) {
-   dir = path.resolve(dir, argv._[0]);
-}
-
-console.log(chalk.green("Creating module..."));
-
-vfs
-  .src(path.join(__dirname, "templates", "**", "*"), { dot: true })
-  .pipe(map(function(file, callback) {
-    file.contents = new Buffer(template(file.contents)({ name: path.basename(dir) }));
-    callback(null, file);
-  }))
-  .once("error", function() {
-    console.error(chalk.red("An error occurred."));
-    process.exit(1);
-  })
-  .pipe(vfs.dest(dir))
-  .pipe(map(function(file, callback) {
-    console.log(chalk.green("+", tildify(file.path)));
-    callback();
-  }))
-  .once("end", function() {
-    console.log(chalk.green("Module created!"));
-    process.exit();
+    fs
+    .src(path.join(__dirname, "templates", "**", "*"), { dot: true })
+    .pipe(map(function(file, cb) {
+      file.contents = new Buffer(template(file.contents)({ name: path.basename(dir) }));
+      cb(null, file);
+    }))
+    .once("error", reject)
+    .pipe(fs.dest(dir))
+    .pipe(map(function(file, cb) {
+      files.push(file.path);
+      cb();
+    }))
+    .once("end", function() {
+      resolve(files);
+    });
   });
+};
