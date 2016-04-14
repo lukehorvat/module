c35d152dec986a93998c12b000a663b35667c758
Luke Horvat <lukehorvat@gmail.com>
2016-04-14 23:57:51 +1000

    Use concat-stream

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 "use strict";
 
 var path = require("path");
+var concat = require("concat-stream")
 var template = require("lodash.template");
 var map = require("map-stream");
 var fs = require("vinyl-fs");
 
-module.exports = function(dir) {
-  return new Promise(function(resolve, reject) {
-    var files = [];
+module.exports = createModule;
 
+function createModule(dir) {
+  return new Promise(function(resolve, reject) {
     fs
     .src(path.join(__dirname, "templates", "**", "*"), { dot: true })
-    .pipe(map(function(file, cb) {
-      file.contents = new Buffer(template(file.contents)({ name: path.basename(dir) }));
-      cb(null, file);
-    }))
+    .pipe(templateFiles({ name: path.basename(dir) }))
     .once("error", reject)
     .pipe(fs.dest(dir))
-    .pipe(map(function(file, cb) {
-      files.push(file.path);
-      cb();
-    }))
-    .once("end", function() {
-      resolve(files);
-    });
+    .pipe(collectFiles(resolve));
   });
-};
+}
+
+function templateFiles(data) {
+  return map(function(file, cb) {
+    file.contents = new Buffer(template(file.contents)(data));
+    cb(null, file);
+  });
+}
+
+function collectFiles(cb) {
+  return concat(function(files) {
+    cb(files.map(function(file) { return file.path; }));
+  });
+}
